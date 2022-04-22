dce0852d9359d5043adf67f711605ef69d09ecbe
CongLuong <66084438+HoCongLuong@users.noreply.github.com>
2022-04-22 23:16:06 +0700

    Update models.py
    Cập nhật file models.py sau khi hoàn thành video thứ 9

--- Midterm/accounts/models.py
+++ Midterm/accounts/models.py
@@ -1,4 +1,3 @@
-from statistics import mode
 from django.db import models
 
 # Create your models here.
@@ -9,7 +8,6 @@ class Customer(models.Model):
 	email = models.CharField(max_length=200, null=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 
-
 	def __str__(self):
 		return self.name
 
@@ -19,7 +17,7 @@ class Tag(models.Model):
 
 	def __str__(self):
 		return self.name
- 
+
 class Product(models.Model):
 	CATEGORY = (
 			('Indoor', 'Indoor'),
